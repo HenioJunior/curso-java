e0abe6da3e8a9a8cb8b403653090e14e1b2be932
Henio Junior <henioajunior@gmail.com>
2021-05-22 18:00:55 -0300

    67. Começando a resolver um segundo problema exemplo

---
+++ src/main/java/exercicio_product/application/Program.java
@@ -0,0 +1,28 @@
+package exercicio_product.application;
+
+import exercicio_product.entities.Product;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter Product Data");
+        System.out.println("Name:");
+        product.name = sc.nextLine();
+
+        System.out.println("Price:");
+        product.price = sc.nextDouble();
+
+        System.out.println("Quantity in stock");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+
+        sc.close();
+    }
+}
