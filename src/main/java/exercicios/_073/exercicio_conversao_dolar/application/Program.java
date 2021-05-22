ec7bd6b9b3de821cd82c9b830265eb7097e4bd2b
Henio Junior <henioajunior@gmail.com>
2021-05-22 18:33:58 -0300

    73. Exercício conversão dolar

---
+++ src/main/java/exercicios/_073/exercicio_conversao_dolar/application/Program.java
@@ -0,0 +1,21 @@
+package exercicios._073.exercicio_conversao_dolar.application;
+
+import exercicios._073.exercicio_conversao_dolar.util.CurrencyConverter;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the dollar price?");
+        double dollarPrice = sc.nextDouble();
+
+        System.out.println("How many dollars will be bought?");
+        double amount = sc.nextDouble();
+
+        System.out.printf("Amount to be paid in reais = %.2f", CurrencyConverter.converter(dollarPrice, amount));
+
+        sc.close();
+    }
+}
---
+++ src/main/java/exercicios/_073/exercicio_conversao_dolar/util/CurrencyConverter.java
@@ -0,0 +1,10 @@
+package exercicios._073.exercicio_conversao_dolar.util;
+
+public class CurrencyConverter {
+
+    public static final double IOF = 1 + 6.0/100;
+
+    public static double converter(double dollarPrice, double amount) {
+        return amount * dollarPrice * IOF ;
+    }
+}
