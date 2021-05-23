29c91ca483cfbec3a424a2ecb0a36cd972596599
Henio Junior <henioajunior@gmail.com>
2021-05-22 22:44:14 -0300

    76. Construtores

---
+++ src/main/java/construtores/application/Program.java
@@ -0,0 +1,46 @@
+package construtores.application;
+
+import construtores.entities.Product;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Product Data");
+        System.out.println("Name:");
+        String name = sc.nextLine();
+
+        System.out.println("Price:");
+        double price = sc.nextDouble();
+
+        System.out.println("Quantity in stock");
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data:"+product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock:");
+        quantity = sc.nextInt();
+        product.addProduct(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: "+product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock:");
+        quantity = sc.nextInt();
+        product.removeProduct(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: "+product);
+
+        sc.close();
+    }
+}
---
+++ src/main/java/construtores/entities/Product.java
@@ -0,0 +1,39 @@
+package construtores.entities;
+
+public class Product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public int addProduct(int quantity) {
+        return this.quantity += quantity;
+    }
+
+    public int removeProduct(int quantity) {
+        return this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                +", $ "
+                +String.format("%.2f", price)
+                +", "
+                +quantity
+                +" units, Total: $ "
+                +String.format("%.2f", totalValueInStock());
+    }
+}
