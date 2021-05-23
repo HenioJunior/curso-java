31cf2070a6b809e9c8255535c47a88345406ae83
Henio Junior <henioajunior@gmail.com>
2021-05-23 08:21:28 -0300

    78. Sobrecarga

---
+++ src/main/java/sobrecarga/application/Program.java
@@ -0,0 +1,45 @@
+package sobrecarga.application;
+
+import sobrecarga.entities.Product;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Product p = new Product();
+
+        System.out.println("Enter Product Data");
+        System.out.println("Name:");
+        String name = sc.nextLine();
+
+        System.out.println("Price:");
+        double price = sc.nextDouble();
+
+        Product product = new Product(name, price);
+
+        System.out.println();
+        System.out.println("Product data:"+product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock:");
+        int quantity = sc.nextInt();
+        product.addProduct(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: "+product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock:");
+        quantity = sc.nextInt();
+        product.removeProduct(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: "+product);
+
+        sc.close();
+    }
+}
