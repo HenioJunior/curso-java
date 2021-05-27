abcb4e438dfb8889d8dc15d633af702745a4c8eb
Henio Junior <henioajunior@gmail.com>
2021-05-27 12:56:07 -0300

    126. Upcasting e downcasting

---
+++ src/main/java/upcasting_downcasting/application/Program.java
@@ -0,0 +1,45 @@
+package upcasting_downcasting.application;
+
+import upcasting_downcasting.entities.Account;
+import upcasting_downcasting.entities.BusinessAccount;
+import upcasting_downcasting.entities.SavingsAccount;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Account acc = new Account(1001, "Alex", 0.0);
+
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        //UPCASTING
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 500.0);
+        Account acc3 = new SavingsAccount(1004, "Antonio", 0.0, 100.0);
+
+        //DOWNCASTING
+
+//        BusinessAccount acc4 = acc2; Não posso converter o Account para BusinessAccount
+//        Para isso, é necessário o downcasting;
+
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0);
+
+//        BusinessAccount acc5 = (BusinessAccount)acc3;
+//        Como o acc3 é do tipo SavingAccount, para evitar erro será necessário testar usando o instanceof;
+
+        if(acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if(acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+
+        }
+    }
+}
