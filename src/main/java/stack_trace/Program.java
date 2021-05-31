8634bd479d29c68dafeabba38a5bb45180ed69e1
Henio Junior <henioajunior@gmail.com>
2021-05-31 10:01:04 -0300

    140. Pilha de chamadas de métodos (stack trace)

---
+++ src/main/java/stack_trace/Program.java
@@ -0,0 +1,40 @@
+package stack_trace;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        method1();
+        System.out.println("End of program");
+
+    }
+    public static void method1() {
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 FINISH***");
+    }
+
+    public static void method2() {
+        System.out.println("***METHOD2 START***");
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("input names: ");
+            String[] vect = sc.nextLine().split(" ");
+            System.out.print("select one: ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+            e.printStackTrace();
+            sc.next();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+        sc.close();
+        System.out.println("***METHOD2 FINISH***");
+    }
+}
