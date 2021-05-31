9c3ba5c634b0ec221a00d524b6c7307daa287f3a
Henio Junior <henioajunior@gmail.com>
2021-05-31 15:09:23 -0300

    146. Exercício de fixação - Account

---
+++ src/main/java/exercicios/_146_account/application/Program.java
@@ -0,0 +1,38 @@
+package exercicios._146_account.application;
+
+import exercicios._146_account.model.entities.Account;
+import exercicios._146_account.model.exceptions.DomainException;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        Integer number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Holder: ");
+        String holder = sc.nextLine();
+        System.out.print("Initial Balance: ");
+        Double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        Double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+
+        try {
+            System.out.print("Enter amount for withdraw:");
+            Double withdraw = sc.nextDouble();
+            account.withdraw(withdraw);
+            System.out.print(account);
+        } catch (DomainException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+        sc.close();
+    }
+}
