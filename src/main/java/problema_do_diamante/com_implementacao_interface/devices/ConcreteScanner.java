6613cc68d6aa92cfc86f9eee0fc293fd7a1fac43
Henio Junior <henioajunior@gmail.com>
2021-06-01 19:40:21 -0300

    200. Herança múltipla e o problema do diamante

---
+++ src/main/java/problema_do_diamante/com_implementacao_interface/devices/ConcreteScanner.java
@@ -0,0 +1,18 @@
+package problema_do_diamante.com_implementacao_interface.devices;
+
+public class ConcreteScanner extends Device implements Scanner {
+
+	public ConcreteScanner(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Scanner processing: " + doc);
+	}
+
+	@Override
+	public String scan() {
+		return "Scanned content";
+	}
+}
---
+++ src/main/java/problema_do_diamante/com_implementacao_interface/devices/Printer.java
@@ -0,0 +1,6 @@
+package problema_do_diamante.com_implementacao_interface.devices;
+
+public interface Printer {
+
+	void print(String doc);
+}
